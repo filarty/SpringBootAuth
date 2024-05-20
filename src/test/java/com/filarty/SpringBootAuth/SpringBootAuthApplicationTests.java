2079524f0a413405f6c13574d9cb0686f19067fb
Artjom <filarty@gmail.com>
2024-05-20 14:09:49 +0200

    add tests

--- src/test/java/com/filarty/SpringBootAuth/SpringBootAuthApplicationTests.java
+++ src/test/java/com/filarty/SpringBootAuth/SpringBootAuthApplicationTests.java
@@ -1,13 +1,27 @@
 package com.filarty.SpringBootAuth;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringBootAuthApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mockMvc.perform(get("/auth/test").contentType(MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value("filarty"));
 	}
 
 }
